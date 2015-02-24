24e416cf0c6e06e9463af4cfe4f367b03f11f6a6
Teddy Engel <engel.teddy@gmail.com>
2015-02-24 15:52:39 +0000

    - Finished implementation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 Teddy Engel. All rights reserved.
 //
 
+#include <vector>
 #include <iostream>
 
 void PrintVector(std::vector<int>& values)
@@ -18,9 +19,29 @@ void PrintVector(std::vector<int>& values)
     }
 }
 
+void Swap(std::vector<int>& values, const int& n1, const int& n2)
+{
+    int nTmp = values[n1];
+    values[n1] = values[n2];
+    values[n2] = nTmp;
+}
+
 void BubbleSort(std::vector<int>& values)
 {
+    bool bSwapped = true;
 
+    while (bSwapped)
+    {
+        bSwapped = false;
+        for (int i = 0; i < values.size() - 1; i++)
+        {
+            if (values[i] > values[i + 1])
+            {
+                Swap(values, i, i + 1);
+                bSwapped = true;
+            }
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
